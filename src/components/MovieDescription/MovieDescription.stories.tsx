bb5424c8e1c616d41482da10d1f6a00a07236302
Cristhian Murcia <mugar97@gmail.com>
2023-05-08 23:53:09 -0500

    MovieDescription stories and styles

--- src/components/MovieDescription/MovieDescription.stories.tsx
+++ src/components/MovieDescription/MovieDescription.stories.tsx
@@ -1,30 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { MovieTile } from './MovieTile';
+import { MovieDescription } from './MovieDescription';
 import { PULP_FICTION, BOHEMIAN_RHAPSODY } from './../../utils/testData';
 
 const meta = {
-  title: 'Movie/MovieTile',
-  component: MovieTile,
-} satisfies Meta<typeof MovieTile>;
+  title: 'Movie/MovieDescription',
+  component: MovieDescription,
+} satisfies Meta<typeof MovieDescription>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const demoOnClick = () => {
-  console.log('Movie clicked!');
-};
-
 export const PulpFiction: Story = {
   args: {
     movie: PULP_FICTION,
-    onClick: demoOnClick,
   },
 };
 
 export const BohemianRhapsody: Story = {
   args: {
     movie: BOHEMIAN_RHAPSODY,
-    onClick: demoOnClick,
   },
 };
--- src/components/MovieDescription/MovieDescription.tsx
+++ src/components/MovieDescription/MovieDescription.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { IMovie } from '../../utils/interfaces';
 import _ from 'lodash';
+import './MovieDescription.scss';
 
 interface MovieDescriptionProps {
   movie: IMovie;
@@ -16,17 +17,29 @@ export class MovieDescription extends Component<MovieDescriptionProps> {
     return (
       <div className='MovieDescription'>
         <div className='MovieDescription__leftPanel'>
-          <img alt={this.movie.name} src={this.movie.imageUrl} />
+          <img
+            className='MovieDescription__leftPanel__image'
+            alt={this.movie.name}
+            src={this.movie.imageUrl}
+          />
         </div>
         <div className='MovieDescription__rightPanel'>
-          <h1>{this.movie.name}</h1>
-          <span title='genres'>
-            {this.movie.genres.map((genre) => _.startCase(genre)).join(', ')}
+          <h1 className='MovieDescription__rightPanel__title'>{this.movie.name}</h1>
+          <span className='MovieDescription__rightPanel__rating' title='rating'>
+            {this.movie.raiting}
           </span>
-          <span title='rating'>{this.movie.raiting}</span>
-          <span title='release-year'>{this.movie.releaseYear}</span>
-          <span title='length'>{this.movie.length}</span>
-          <p role='paragraph'>{this.movie.description}</p>
+          <span className='MovieDescription__rightPanel__genres' title='genres'>
+            {this.movie.genres.map((genre) => genre.replace(/\w+/g, _.capitalize)).join(', ')}
+          </span>
+          <span className='MovieDescription__rightPanel__year' title='release-year'>
+            {this.movie.releaseYear}
+          </span>
+          <span className='MovieDescription__rightPanel__length' title='length'>
+            {this.movie.length}
+          </span>
+          <p className='MovieDescription__rightPanel__description' role='paragraph'>
+            {this.movie.description}
+          </p>
         </div>
       </div>
     );
